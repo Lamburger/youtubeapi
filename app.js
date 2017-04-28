c1f80de1178ea9fd7b58814e04d498df71d80b11
Lamburger <rival2h@gmail.com>
2017-04-27 20:36:53 -0700

    cats debugged

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function displayYoutubeSearchData(data) {
   var resultElement = '';
   if (data.items) {
     data.items.forEach(function(item) {
-     resultElement += '<p>' + item.snippet.title + '</p>';
+     resultElement += '<p>' + item.snippet.title + '</p>' + '<a href = "https://www.youtube.com/watch?v='+item.id.videoId +'" >' + '<img src = ' + item.snippet.thumbnails.default.url + '>' + '</a>';
     });
   }
   else {
